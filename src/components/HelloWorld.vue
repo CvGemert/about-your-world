11cf4a36a3822841e3e7f4267306dba94a04aed6
CvGemert <caspervangemert@gmail.com>
2020-09-27 15:18:56 +0200

    Added axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,9 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        Base component
+        <v-card>
+          {{sources}}
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -13,7 +15,21 @@ export default {
   name: "HelloWorld",
 
   data() {
-    return {};
+    return {
+      sources: null,
+      selectedIndicator: 44206,
+    };
+  },
+  created() {
+    this.axios
+      .get(
+        "http://ec2-54-174-131-205.compute-1.amazonaws.com/API/HDRO_API.php/indicator_id=" +
+          this.selectedIndicator
+      )
+      .then((resp) => {
+        this.sources = [resp.data.indicator_value];
+      })
+      .finally(() => (this.loading = false));
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import VueApexCharts from 'vue-apexcharts'
+
+// Axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 
 // Apexcharts
+import VueApexCharts from 'vue-apexcharts'
 Vue.use(VueApexCharts);
 Vue.component('apexchart', VueApexCharts)
 
